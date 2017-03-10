ca55a257c8d26ccb384df1ab6067a846c9b50a72
Theodore M Miller <theomil@caen-vnc-vm12.engin.umich.edu>
2017-03-10 14:50:05 -0500

    syntax errors, need to look into

--- rpn.py
+++ rpn.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
+def add(arg1, arg2):
+	return arg1 + arg2
+
 def calculate(arg):
-	pass
+	stack = list()
+	for operand in arg.split():
+		if operand == '+':
+			stack.append(stack.pop() + stack.pop())
+		else:
+			stack.append(float(operand))
 def main():
 	while True:
 		calculate(input("rpn calc> "))
